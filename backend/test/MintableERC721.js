9529bec800d7119728734165296a3743c853e9cc
Justus Perlwitz <mail@justusperlwitz.com>
2018-04-24 14:54:26 +0900

    Backend: Test token transferability

--- backend/test/MintableERC721.js
+++ backend/test/MintableERC721.js
@@ -5,27 +5,52 @@ const assertRejected = require("assert-rejected");
 const Token = artifacts.require("contracts/MintableERC721.sol");
 
 contract("MintableERC721", accounts => {
+  const account = accounts[0];
+  const otherAccount = accounts[1];
+  const thirdAccount = accounts[2];
+  const options = {from: account};
+  const otherOptions = {from: otherAccount};
+  const thirdOptions = {from: thirdAccount};
+
+  const tokenId = 100;
+
   let token;
-
-  let options = {from: accounts[0]};
-  let otherOptions = {from: accounts[1]};
-
-  before(async () => {
-  });
 
   beforeEach(async () => {
     token = await Token.new();
   });
 
   it("should be created correctly", async () => {
-    assert.equal(await token.owner(), accounts[0]);
+    assert.equal(await token.owner(), account);
   });
 
-  it("will let the owner mint", async () => {
-    await token.mint(accounts[1], 0, options);
+  describe("minting", () => {
+    it("will let the owner mint", async () => {
+      await token.mint(account, tokenId, options);
+    });
+
+    it("won't let others mint", async () => {
+      await assertRejected(token.mint(otherAccount, tokenId, otherOptions));
+    });
   });
 
-  it("won't let others mint", async () => {
-    await assertRejected(token.mint(accounts[1], 0, otherOptions));
+  describe("transferring", () => {
+    beforeEach(async () => {
+      await token.mint(account, tokenId);
+    });
+
+    it("lets users transfer tokens", async () => {
+      await token.approve(otherAccount, tokenId, options);
+      await token.transferFrom(
+        account, otherAccount, tokenId, otherOptions,
+      );
+    });
+
+    it("won't let the wrong user transfer", async () => {
+      await token.approve(otherAccount, tokenId, options);
+      await assertRejected(token.transferFrom(
+        account, otherAccount, tokenId, thirdOptions,
+      ));
+    });
   });
 });
