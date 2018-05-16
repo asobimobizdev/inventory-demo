ffe8f01a4326468cbede22a86f75674417e76565
Justus <mail@justusperlwitz.com>
2018-05-16 16:25:42 +0900

    Contracts/Trade: Add/Remove Goods

--- contracts/Trade.sol
+++ contracts/Trade.sol
@@ -5,18 +5,62 @@ import "contracts/Goods.sol";
 
 contract Trade {
 
+    event GoodAdded(address _trader, uint256 goodID);
+
     address[] public traders;
+    mapping(address=>bool) public isTrader;
 
     Goods goods;
 
-    mapping(address => uint256[]) offers;
+    mapping(address => uint256[]) public traderGoods;
+    mapping(uint256 => uint256) public traderGoodsIndex;
+    uint256 public numGoods = 0;
 
     constructor(Goods _goods, address[] _traders) public {
         goods = _goods;
         traders = _traders;
+        for (uint256 index = 0; index < traders.length; index++) {
+            isTrader[traders[index]] = true;
+        }
+    }
+
+    modifier onlyTrader(address trader) {
+        require(isTrader[trader]);
+        _;
     }
 
     function numTraders() public view returns (uint256) {
         return traders.length;
+    }
+
+    function addGood(uint256 goodID) onlyTrader(msg.sender) public {
+        require(goods.ownerOf(goodID) == msg.sender);
+
+        traderGoodsIndex[goodID] = traderGoods[msg.sender].length;
+        traderGoods[msg.sender].push(goodID);
+        numGoods++;
+
+        emit GoodAdded(msg.sender, goodID);
+    }
+
+    function removeGood(uint256 goodID) onlyTrader(msg.sender) public {
+        require(goods.ownerOf(goodID) == msg.sender);
+        uint index = traderGoodsIndex[goodID];
+
+        for (uint i = index; i < traderGoods[msg.sender].length - 1; i++){
+            traderGoods[msg.sender][i] = traderGoods[msg.sender][i + 1];
+        }
+        traderGoods[msg.sender].length--;
+        numGoods--;
+    }
+
+    function numTraderGoods(address trader)
+    onlyTrader(trader) public view returns (uint256) {
+        return traderGoods[trader].length;
+    }
+
+    function traderGoodByIndex(address trader, uint256 index)
+    onlyTrader(trader) public view returns (uint256) {
+        return traderGoods[trader][index];
     }
 }
--- test/Trade.js
+++ test/Trade.js
@@ -6,25 +6,100 @@ const Goods = artifacts.require("contracts/Goods.sol");
 const Trade = artifacts.require("contracts/Trade.sol");
 
 contract("Trade", accounts => {
-  const playerA = accounts[1];
-  const playerB = accounts[2];
-  const playerAOptions = {from: playerA};
-  const playerBOptions = {from: playerB};
+  const traderA = accounts[1];
+  const traderB = accounts[2];
+  const traderAOptions = {from: traderA};
+  const traderBOptions = {from: traderB};
+  const thirdPerson = accounts[3];
+
+  const item1 = 1;
+  const item2 = 2;
+  const item3 = 3;
 
   let goods;
   let trade;
 
   beforeEach(async () => {
     goods = await Goods.new();
-    trade = await Trade.new(goods.address, [playerA, playerB]);
-    // await goods.mint(seller, goodID);
+    trade = await Trade.new(goods.address, [traderA, traderB]);
   });
 
   it("can be created", async () => {
-    assert.equal(await trade.traders(0), playerA);
+    assert.equal(await trade.traders(0), traderA);
   });
 
   it("returns the correct number of traders", async () => {
     assert.equal(await trade.numTraders(), 2);
+  });
+
+  it("lets users check if they are traders", async () => {
+    assert.isTrue(await trade.isTrader(traderA));
+  });
+
+  describe("adding goods", () => {
+    beforeEach(async () => {
+      await goods.mint(traderA, item1);
+      await goods.mint(traderB, item2);
+      await goods.mint(traderB, item3);
+
+      await goods.approve(trade.address, item1, traderAOptions);
+      await goods.approve(trade.address, item2, traderBOptions);
+      await goods.approve(trade.address, item3, traderBOptions);
+    });
+
+    it("has no goods in the beginning", async () => {
+      assert.equal(await trade.numGoods(), 0);
+    });
+
+    it("lets traders add goods", async () => {
+      await trade.addGood(item1, traderAOptions);
+      assert.equal((await trade.numGoods()).toNumber(), 1);
+    });
+
+    it("checks ownership", async () => {
+      await assertRejected(trade.addGood(item1, traderBOptions));
+    });
+
+    describe("with one good added", () => {
+      beforeEach(async () => {
+        await trade.addGood(item1, traderAOptions);
+      });
+
+      it("knows how many trades one trader has", async () => {
+        assert.equal(await trade.numTraderGoods(traderA), 1);
+        await assertRejected(trade.numTraderGoods(thirdPerson));
+      });
+
+      it("keeps track of the individual goods offered", async () => {
+        assert.equal(await trade.traderGoodByIndex(traderA, 0), item1);
+      });
+
+      it("lets you remove goods", async () => {
+        await trade.removeGood(item1, traderAOptions);
+        assert.equal((await trade.numTraderGoods(traderA)).toNumber(), 0);
+        assert.equal((await trade.numGoods()).toNumber(), 0);
+      });
+    });
+
+    describe("with all goods added", () => {
+      beforeEach(async () => {
+        await trade.addGood(item1, traderAOptions);
+        await trade.addGood(item2, traderBOptions);
+        await trade.addGood(item3, traderBOptions);
+      });
+
+      it("keeps correct counts", async () => {
+        assert.equal((await trade.numTraderGoods(traderB)).toNumber(), 2);
+        assert.equal((await trade.numGoods()).toNumber(), 3);
+
+        await trade.removeGood(item2, traderBOptions);
+        assert.equal((await trade.numTraderGoods(traderB)).toNumber(), 1);
+        assert.equal((await trade.numGoods()).toNumber(), 2);
+
+        await trade.removeGood(item3, traderBOptions);
+        assert.equal((await trade.numTraderGoods(traderB)).toNumber(), 0);
+        assert.equal((await trade.numGoods()).toNumber(), 1);
+      });
+    });
   });
 });
