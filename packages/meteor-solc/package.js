2622960b7f91e250290f67f1719c7b8eeb90d833
Justus <mail@justusperlwitz.com>
2018-05-23 15:48:56 +0900

    Implement solc caching
    Fix #54

--- packages/meteor-solc/package.js
+++ packages/meteor-solc/package.js
@@ -1,25 +1,14 @@
 Package.describe({
   name: "asobicoin:solc",
-  summary: "The solc package provides a compiler build plugin for the Meteor build tool.",
-  version: "0.4.2",
-  git: "http://github.com/SilentCicero/meteor-solc",
+  summary: "Compile solc packages",
+  version: "0.4.23",
 });
 
 Package.registerBuildPlugin({
   name: "compileSol",
-  use: [
-    "ecmascript@0.1.0",
-    "isobuild:compiler-plugin@1.0.0",
-    "caching-compiler@1.0.0",
-  ],
-  sources: [
-    "plugin/handler.js",
-  ],
-  npmDependencies: {
-    "solc": "0.4.23",
-  },
+  use: ["caching-compiler@1.0.0"],
+  sources: ["plugin/handler.js"],
+  npmDependencies: {"solc": "0.4.23"},
 });
 
-Package.onUse(function (api) {
-  api.use("isobuild:compiler-plugin@1.0.0");
-});
+Package.onUse((api) => api.use("isobuild:compiler-plugin@1.0.0"));
--- packages/meteor-solc/plugin/handler.js
+++ packages/meteor-solc/plugin/handler.js
@@ -1,9 +1,25 @@
+/* globals CachingCompiler */
 const fs = Npm.require("fs");
 const path = Npm.require("path");
 const solc = Npm.require("solc");
 
 
-class SolidityCompiler {
+class SolidityCompiler extends CachingCompiler {
+  constructor() {
+    super({
+      compilerName: "solidityCompiler",
+      defaultCacheSize: 1024 * 1024 * 10,
+    });
+  }
+
+  getCacheKey(inputFile) {
+    return inputFile.getSourceHash();
+  }
+
+  compileResultSize(compileResult) {
+    return compileResult.source.length;
+  }
+
   findImports(p) {
     const read = (p) => {
       return {
@@ -27,66 +43,64 @@ class SolidityCompiler {
   // Refer to
   // https://solidity.readthedocs.io/en/develop/using-the-compiler.html#compiler-input-and-output-json-description
   // https://docs.meteor.com/api/packagejs.html#build-plugin-compilers
-  processFilesForTarget(files) {
-    files.forEach((file) => {
-      const fileName = file.getPathInPackage();
-      if (/node_modules\/.+sol/.test(fileName)) {
-        return;
-      }
+  compileOneFile(file) {
+    const fileName = file.getPathInPackage();
+    if (/node_modules\/.+sol/.test(fileName)) {
+      return;
+    }
 
-      const input = {
-        language: "Solidity",
-        sources: {
-          "file": {
-            content: file.getContentsAsString(),
+    const input = {
+      language: "Solidity",
+      sources: {
+        "file": {
+          content: file.getContentsAsString(),
+        },
+      },
+      settings: {
+        evmVersion: "byzantium",
+        outputSelection: {
+          // Enable the metadata and bytecode outputs of every single contract.
+          "*": {
+            "*": [
+              "abi", "evm.bytecode.object",
+            ],
           },
         },
-        settings: {
-          evmVersion: "byzantium",
-          outputSelection: {
-            // Enable the metadata and bytecode outputs of every single contract.
-            "*": {
-              "*": [
-                "abi", "evm.bytecode.object",
-              ],
-            },
-          },
-        },
-      };
+      },
+    };
 
-      const output = solc.compileStandardWrapper(
-        JSON.stringify(input),
-        this.findImports,
-      );
+    const output = solc.compileStandardWrapper(
+      JSON.stringify(input),
+      this.findImports,
+    );
 
-      const outData = JSON.parse(output);
-      for (let error of outData.errors || []) {
-        if (error.severity == "error") {
-          file.error({
-            message: error.message,
-            column: error.sourceLocation.start,
-          });
-        }
+    const outData = JSON.parse(output);
+    for (let error of outData.errors || []) {
+      if (error.severity == "error") {
+        file.error({
+          message: error.message,
+          column: error.sourceLocation.start,
+        });
       }
-      let jsContent = "module.exports = {";
-      for (let contractFile in outData.contracts) {
-        const contracts = outData.contracts[contractFile];
-        contractFile = contractFile.split("/").pop().split(".sol")[0];
-        for (let contractName in contracts) {
-          const contract = contracts[contractName];
-          const contractOut = {
-            abi: contract.abi,
-            bytecode: contract.evm.bytecode.object,
-          };
-          jsContent += `${contractName}: ${JSON.stringify(contractOut)},\n`;
-        }
+    }
+    let jsContent = "module.exports = {";
+    for (let contractFile in outData.contracts) {
+      const contracts = outData.contracts[contractFile];
+      contractFile = contractFile.split("/").pop().split(".sol")[0];
+      for (let contractName in contracts) {
+        const contract = contracts[contractName];
+        const contractOut = {
+          abi: contract.abi,
+          bytecode: contract.evm.bytecode.object,
+        };
+        jsContent += `${contractName}: ${JSON.stringify(contractOut)},\n`;
       }
-      jsContent += "};";
+    }
+    jsContent += "};";
 
-      file.addJavaScript({
-        data: jsContent,
-        path: `${file.getPathInPackage()}.js`,
-      });
+    file.addJavaScript({
+      data: jsContent,
+      path: `${file.getPathInPackage()}.js`,
     });
   }
 }
