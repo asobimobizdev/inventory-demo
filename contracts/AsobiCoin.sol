4d18e0373db2ab9d906abba580db14fd50573fe8
Justus Perlwitz <mail@justusperlwitz.com>
2018-06-22 10:55:31 +0900

    Contracts: Switch AsobiCoin to ERC827

--- contracts/AsobiCoin.sol
+++ contracts/AsobiCoin.sol
@@ -1,12 +1,13 @@
 pragma solidity ^0.4.22;
 
-import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC827/ERC827Token.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
 
 /**
  * @title AsobiCoin is the MintableToken implementation for testing AsobiCoin
  */
-contract AsobiCoin is MintableToken {
+contract AsobiCoin is ERC827Token, MintableToken {
     string public name = "ASOBI COIN";
     string public symbol = "ASC";
     uint256 public decimals = 18;
