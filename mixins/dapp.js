dedc1a626530dbf8b67b2879a6996b318683bd60
Justus Perlwitz <mail@justusperlwitz.com>
2018-06-26 10:30:26 +0900

    Mixins: Make dapp initialization more robust

--- mixins/dapp.js
+++ mixins/dapp.js
@@ -4,17 +4,23 @@ export default {
   async fetch({ store, params }) {
     try {
       await dapp.asyncInitialize();
+      store.commit("accountAddress", dapp.defaultAccount);
     } catch(e) {
       console.error("Network issue", e);
       return;
     }
-    store.commit("accountAddress", dapp.defaultAccount);
-    store.dispatch("getGoodsContract");
-    store.dispatch("getAsobiCoinContract");
-    store.dispatch("getEscrowContract");
-    store.dispatch("getShopContract");
-    store.dispatch("getTradeRegistryContract");
-    store.dispatch("getUserRegistryContract");
+    try {
+      store.dispatch("getGoodsContract");
+      store.dispatch("getAsobiCoinContract");
+      store.dispatch("getEscrowContract");
+      store.dispatch("getShopContract");
+      store.dispatch("getTradeRegistryContract");
+      store.dispatch("getUserRegistryContract");
+    } catch(e) {
+      console.error("Address issue", e);
+      return;
+    }
+
     try {
       await Promise.all([
         store.dispatch("getBalance"),
@@ -24,6 +30,7 @@ export default {
       ]);
     } catch(e) {
       console.error("Could not access contracts", e);
+      return;
     }
     store.commit("dapp/initialized", true);
   },
